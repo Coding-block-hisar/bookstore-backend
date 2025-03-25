0159c786135af5cc6b1b087ff39e5b85cd9baf5a
akash kumar <65843349+Coding-block-hisar@users.noreply.github.com>
2025-03-25 12:52:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(cors(
     {
-        origin: ["https://bookstore-frontend-steel.vercel.app"],
+        origin: ["https://bookstore-frontend-steel.vercel.app/"],
         methods: ["POST", "GET","PUT","DELETE"],
         credentials: true
     }
