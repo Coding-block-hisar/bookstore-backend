f2c55d85e1df17bc6e045ae92a9c28a079900f79
akash kumar <65843349+Coding-block-hisar@users.noreply.github.com>
2025-03-25 13:16:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Middlewares
 app.use(express.json());
-// app.use(cors());
+ app.use(cors());
 
 // app.use(cors({
 //   origin: 'https://bookstore-frontend-steel.vercel.app' // specify the frontend URL
