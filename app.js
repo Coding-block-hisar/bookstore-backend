d7f4160422e02d0cbf1f2b8a99c4989f4cb6e2a2
akash kumar <65843349+Coding-block-hisar@users.noreply.github.com>
2025-03-25 10:56:13 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-routes");
+const cors = require("cors");
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/books", router); // localhost:5000/books
+
+// mongoose
+//   .connect(
+//     "mongodb://127.0.0.1:27017/bookstore"
+//   )
+//   .then(() => console.log("Connected To Database"))
+//   .then(() => {
+//     app.listen(5000);
+//   })
+//   .catch((err) => console.log(err));
+
+
+mongoose
+  .connect(
+    'mongodb+srv://codingblockindia:2i7wGs34RYHsdQ0P@contact.ewc0xy6.mongodb.net/?retryWrites=true&w=majority&appName=contact'
+  )
+  .then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
